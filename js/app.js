95df08b369b3e7fe965ffe3b5dbdd0d5f26cc5c2
Myamlya <66949526+myamlya@users.noreply.github.com>
2020-12-27 00:59:35 +0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,41 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+	// Custom JS
+
+	const body = document.querySelector('body');
+
+	cx = window.innerHeight / 2;
+	cy = window.innerWidth / 2;
+
+	body.addEventListener('mousemove', e => {
+
+		clientX = e.pageX;
+		clientY = e.pageY;
+
+		//console.log(clientX + '/' + clientY);
+
+		request = requestAnimationFrame(updateMe);
+	})
+
+	function updateMe() {
+
+		dx = clientX - cx
+		dy = clientY - cy
+		tiltx = dy / cy
+		tilty = dx / cx
+		radius = Math.sqrt(Math.pow(tiltx, 2) + Math.pow(tilty, 2))
+		begree = radius * 12 //угол наклона 3d анимации
+		gsap.to('.content', 1, { transform: `rotate3d(${tiltx}, ${tilty}, 0, ${begree}deg)`})
+	}
+
+	gsap.to('.card', { zoom: .98 })	
+	gsap.to('.l', { opacity: 1, duration: .1})	
+	gsap.to('.l2', { opacity: 1, left: -10, top: 10, duration: .3, delay: .25})	
+	gsap.to('.l3', { opacity: 1, left: -20, top: 20, duration: .3, delay: .25})	
+	gsap.to('.card-russia', { opacity: 0.07, duration: .1})	
+	gsap.to('.card-logo-w', { opacity: 1, duration: .3})	
+	gsap.to('.card-valid', { opacity: 1, zoom: 1, delay: .3, duration: 1 })	
+	gsap.to('.card-number-holder', { opacity: 1, zoom: 1, delay: .3, duration: 1 })	
+	gsap.to('.card-chip', { opacity: 1, duration: .3})	
+
+})
